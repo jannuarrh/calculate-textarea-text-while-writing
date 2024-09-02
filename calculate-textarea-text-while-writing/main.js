7219614a0707959c0c1a8dc38c991dc20641580c
jannuarrh <alvinakipure54@gmail.com>
2024-09-02 10:38:35 +0200

    Update main.js

--- calculate-textarea-text-while-writing/main.js
+++ calculate-textarea-text-while-writing/main.js
@@ -17,21 +17,3 @@ myTextarea.onkeyup = function() {
 // document.getElementById('myText').onkeyup = function() {
 // 'use strict';
 
-// mySpan.textContent = 50 - this.value.length;
-// mySpan.textContent < 0 ? mySpan.style.color = '#F00' :  mySpan.style.color = '#000';
-// };/////////////////////
-///////
-//////
-/////
-/
-/
-/
-//////////////
-//
-/
-////////
-///////////////////
-//////////////////
-//////////////////////
-//////////////////////////////////////
-//////////////////
